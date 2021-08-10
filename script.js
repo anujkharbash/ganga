fe24854556dfb45ebf3a3466f60e5d835f9a005f
anujkharbash <87861292+bhorki@users.noreply.github.com>
2021-08-10 09:46:03 +0530

    up

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 let menu = document.querySelector('#menu-bar');
 let navbar = document.querySelector('.navbar');
 let header = document.querySelector('.header-3');
@@ -40,7 +39,7 @@ var swiper = new Swiper(".home-slider", {
     loop:true,
 });
 
-let countDate = new Date('August 10, 2021 19:14:00').getTime();
+let countDate = new Date('june 1, 2021 00:00:00').getTime();
 
 function countDown(){
 
@@ -67,74 +66,4 @@ function countDown(){
 
 setInterval(function(){
     countDown();
-=======
-let menu = document.querySelector('#menu-bar');
-let navbar = document.querySelector('.navbar');
-let header = document.querySelector('.header-3');
-let scrollTop = document.querySelector('.scroll-top');
-
-menu.addEventListener('click', () =>{
-    menu.classList.toggle('fa-times');
-    navbar.classList.toggle('active');
-});
-
-window.onscroll = () =>{
-
-    menu.classList.remove('fa-times');
-    navbar.classList.remove('active');
-
-    if(window.scrollY > 250){
-        header.classList.add('active');
-    }else{
-        header.classList.remove('active');
-    }
-
-    if(window.scrollY > 250){
-        scrollTop.style.display = 'initial';
-    }else{
-        scrollTop.style.display = 'none';
-    }
-
-}
-
-var swiper = new Swiper(".home-slider", {
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
-    autoplay: {
-        delay: 3000,
-        disableOnInteraction: false,
-    },
-    loop:true,
-});
-
-let countDate = new Date('August 10, 2021 19:14:00').getTime();
-
-function countDown(){
-
-    let now = new Date().getTime();
-
-    gap = countDate - now;
-
-    let second = 1000;
-    let minute = second * 60;
-    let hour = minute * 60;
-    let day = hour * 24;
-
-    let d = Math.floor(gap / (day));
-    let h = Math.floor((gap % (day)) / (hour));
-    let m = Math.floor((gap % (hour)) / (minute));
-    let s = Math.floor((gap % (minute)) / (second));
-
-    document.getElementById('day').innerText = d;
-    document.getElementById('hour').innerText = h;
-    document.getElementById('minute').innerText = m;
-    document.getElementById('second').innerText = s;
-
-}
-
-setInterval(function(){
-    countDown();
->>>>>>> 1f840635c2725bc39bfde6764cc42778bbb37488
 },1000)
